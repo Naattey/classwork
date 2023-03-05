d798abca3e6b0fa8361c06874b76941cd6aaeec5
Naattey <taxoost@gmail.com>
2023-03-05 12:02:23 +0500

    test

--- main.py
+++ main.py
@@ -1,17 +1,62 @@
-print("Welcome")
-print("My name is Dhonmohonu")
-print("I'm trainned BOT for calculation purpose")
+# Code 111
+#
+# print("Welcome")
+# print("My name is Dhonmohonu")
+# print("I'm trainned BOT for calculation purpose")
+# userans = int (input("Your Ans: "))
+#
+# def corans():
+#     coranswer = 10
+#     userans = coranswer
+#
+#     print("Correct answer is: ", coranswer )
+#
+# corans()
+#
+# Code 111
+# code 000
+#
+import random
+#limited try
+max = 5
+
+#welcome note
+print('''Welcome to the guess GAME
+
+Please mention your name''')
+player_name = input("User name: ")
+
+# Generate a random number between 1 and 100
+number = random.randint(1, 100)
+
+# Set the initial number of guesses to 0
+num_guesses = 0
+
+# Loop until the player guesses the number
+while True and num_guesses < max:
+    # Ask the player for a guess
+    guess = int(input("Guess a number between 1 and 100: "))
+
+    # Increase the number of guesses by 1
+    num_guesses += 1
+
+    # Check if the guess is correct
+    if guess == number:
+        print("Congratulations, player_name , you guessed the number in", num_guesses, "guesses!")
+        break
+    # Check if the guess is too low
+    if guess < number:
+        print("Too low, try again!")
+    # The guess must be too high
+    else:
+        print("Too high, try again!")
 
 
-userans = int (input("Your Ans: "))
+#
 
-def corans():
-    coranswer = 10
-    userans = coranswer
+#
+# code 000
 
-    print("Correct answer is: ", coranswer )
-
-corans()
 
 # guessseachrange = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 #
@@ -25,34 +70,3 @@ corans()
 # else:
 #     print("Right answer is: ")
 
-
-# code 000
-# import random
-#
-# # Generate a random number between 1 and 100
-# number = random.randint(1, 100)
-#
-# # Set the initial number of guesses to 0
-# num_guesses = 0
-#
-# # Loop until the player guesses the number
-# while True:
-#     # Ask the player for a guess
-#     guess = int(input("Guess a number between 1 and 100: "))
-#
-#     # Increase the number of guesses by 1
-#     num_guesses += 1
-#
-#     # Check if the guess is correct
-#     if guess == number:
-#         print("Congratulations, you guessed the number in", num_guesses, "guesses!")
-#         break
-#     # Check if the guess is too low
-#     elif guess < number:
-#         print("Too low, try again!")
-#     # The guess must be too high
-#     else:
-#         print("Too high, try again!")
-#
-#
-# code 000
